b9c084c14eba54f6c0b3adb8864946a8d92754d6
prodeveloper0 <lsme8821@gmail.com>
2025-05-01 17:04:26 +0900

    feat: implement `HTTPRemoteStore`

--- client/source/remote.cpp
+++ client/source/remote.cpp
@@ -1,13 +1,224 @@
 #include "remote.hpp"
+#include "http.hpp"
+
+#include "utils.hpp"
 
 
-RestRemoteStore::RestRemoteStore(const std::string& serverUrl)
-    : m_serverUrl(serverUrl)
+#define HTTP_REMOTE_SUCCESS (0)
+#define HTTP_REMOTE_NETWORK_ERROR (-1)
+#define HTTP_REMOTE_SERVER_ERROR (-2)
+#define HTTP_REMOTE_FILE_ERROR (-3)
+
+
+HTTPRemoteStore::HTTPRemoteStore(const std::string& serverUrl, const std::string& saveDataPath) 
+    : serverUrl(serverUrl), saveDataPath(saveDataPath)
 {
 }
 
 
-RestRemoteStore::~RestRemoteStore()
+HTTPRemoteStore::~HTTPRemoteStore()
 {
 }
 
+
+int HTTPRemoteStore::_issueSaveDataRevisionId(const std::string userName, const u64 titleId, std::string& revisionId)
+{
+    HTTPClient client;
+    const std::string url = serverUrl + "/users/" + userName + "/saves/" + toHex(titleId) + "/revisions";
+    client.setUrl(url).setMethod("POST")
+    .setReceiveCallback([&revisionId](const void* data, size_t size, size_t& actualSize) {
+        revisionId = std::string(static_cast<const char*>(data), size);
+        actualSize = size;
+        return true;
+    });
+
+    int ret = client.perform();
+
+    if (ret < 0)
+    {
+        return HTTP_REMOTE_NETWORK_ERROR;
+    }
+    else if (ret != 200)
+    {
+        return HTTP_REMOTE_SERVER_ERROR;
+    }
+
+    return HTTP_REMOTE_SUCCESS;
+}
+
+
+int HTTPRemoteStore::_uploadSaveDataRevision(const std::string userName, const u64 titleId, const std::string& revisionId)
+{
+    FILE* file = NULL;
+
+    const Defer defer([&]() {
+        if (file != NULL) 
+        {
+            fclose(file);
+        }
+    });
+
+    const std::string hexTitleId = toHex(titleId);
+    const std::string filePath = saveDataPath + "/" + hexTitleId + ".sar";
+
+    file = fopen(filePath.c_str(), "rb");
+    if (file == NULL) 
+    {
+        return HTTP_REMOTE_FILE_ERROR;
+    }
+
+    HTTPClient client;
+    const std::string url = serverUrl + "/users/" + userName + "/saves/" + hexTitleId + "/revisions/" + revisionId;
+    client.setUrl(url).setMethod("POST")
+    .setSendCallback([&file](void* data, size_t size, size_t& actualSize) {
+        actualSize = fread(data, 1, size, file);
+        if (actualSize < 0)
+        {
+            return false;
+        }
+
+        return true;
+    });
+
+    int ret = client.perform();
+
+    if (ret < 0)
+    {
+        return HTTP_REMOTE_NETWORK_ERROR;
+    }
+    else if (ret != 200)
+    {
+        return HTTP_REMOTE_SERVER_ERROR;
+    }
+
+    return HTTP_REMOTE_SUCCESS;
+}
+
+
+int HTTPRemoteStore::_getLatestSaveDataRevision(const std::string userName, const u64 titleId, std::string& revisionId)
+{
+    HTTPClient client;
+    const std::string url = serverUrl + "/users/" + userName + "/saves/" + std::to_string(titleId) + "/revisions";
+    client.setUrl(url).setMethod("GET")
+    .setReceiveCallback([&revisionId](const void* data, size_t size, size_t& actualSize) {
+        revisionId = std::string(static_cast<const char*>(data), size);
+        actualSize = size;
+        return true;
+    });
+
+    int ret = client.perform();
+
+    if (ret < 0)
+    {
+        return HTTP_REMOTE_NETWORK_ERROR;
+    }
+    else if (ret != 200)
+    {
+        return HTTP_REMOTE_SERVER_ERROR;
+    }
+
+    return HTTP_REMOTE_SUCCESS;
+}
+
+
+int HTTPRemoteStore::_downloadSaveDataRevision(const std::string userName, const u64 titleId, const std::string& revisionId)
+{
+    FILE* file = NULL;
+
+    const Defer defer([&]() {
+        if (file != NULL) {
+            fclose(file);
+        }
+    });
+
+    const std::string filePath = saveDataPath + "/" + std::to_string(titleId) + "/" + revisionId + ".sar";
+    remove(filePath.c_str());
+    file = fopen(filePath.c_str(), "wb");
+    if (file == NULL) 
+    {
+        return HTTP_REMOTE_FILE_ERROR;
+    }
+
+    HTTPClient client;
+    const std::string url = serverUrl + "/users/" + userName + "/saves/" + std::to_string(titleId) + "/revisions/" + revisionId;
+    client.setUrl(url).setMethod("GET")
+    .setReceiveCallback([&file](const void* data, size_t size, size_t& actualSize) {
+        if ((actualSize = fwrite(data, 1, size, file)) < 0)
+        {
+            return false;
+        }
+
+        return true;
+    });
+
+    int ret = client.perform();
+
+    if (ret < 0)
+    {
+        return HTTP_REMOTE_NETWORK_ERROR;
+    }
+    else if (ret != 200)
+    {
+        return HTTP_REMOTE_SERVER_ERROR;
+    }
+
+    return HTTP_REMOTE_SUCCESS;
+}
+
+
+int HTTPRemoteStore::push(const std::string userName, const u64 titleId)
+{
+    int ret;
+    std::string revisionId;
+    
+    ret = _issueSaveDataRevisionId(userName, titleId, revisionId);
+    if (ret != HTTP_REMOTE_SUCCESS)
+    {
+        return -1;
+    }
+
+    return push(userName, titleId, revisionId);
+}
+
+
+int HTTPRemoteStore::pull(const std::string userName, const u64 titleId)
+{
+    int ret;
+    std::string revisionId;
+    
+    ret = _getLatestSaveDataRevision(userName, titleId, revisionId);
+    if (ret != HTTP_REMOTE_SUCCESS)
+    {
+        return -1;
+    }
+
+    return pull(userName, titleId, revisionId);
+}
+
+
+int HTTPRemoteStore::push(const std::string userName, const u64 titleId, const std::string& revision)
+{
+    int ret;
+
+    ret = _uploadSaveDataRevision(userName, titleId, revision);
+    if (ret != HTTP_REMOTE_SUCCESS)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int HTTPRemoteStore::pull(const std::string userName, const u64 titleId, const std::string& revision)
+{
+    int ret;
+
+    ret = _downloadSaveDataRevision(userName, titleId, revision);
+    if (ret != HTTP_REMOTE_SUCCESS)
+    {
+        return -1;
+    }
+
+    return 0;
+}
--- client/source/remote.hpp
+++ client/source/remote.hpp
@@ -8,10 +8,10 @@
 class IRemoteStoreIO
 {
 public:
-    virtual int push(AccountUid accountUid, u64 titleId) = 0;
-    virtual int pull(AccountUid accountUid, u64 titleId) = 0;
-    virtual int push(AccountUid accountUid, u64 titleId, const std::string& revision) = 0;
-    virtual int pull(AccountUid accountUid, u64 titleId, const std::string& revision) = 0;
+    virtual int push(const std::string userName, const u64 titleId) = 0;
+    virtual int pull(const std::string userName, const u64 titleId) = 0;
+    virtual int push(const std::string userName, const u64 titleId, const std::string& revision) = 0;
+    virtual int pull(const std::string userName, const u64 titleId, const std::string& revision) = 0;
 };
 
 
@@ -19,24 +19,31 @@ public:
 class IRemoteStoreQuery
 {
 public:
-    virtual int hasTitle(AccountUid accountUid, u64 titleId) = 0;
-    virtual int queryTitles(std::vector<u64>& outTitles) = 0;
-    virtual int queryRevisions(AccountUid accountUid, u64 titleId, std::vector<std::string>& outRevisions) = 0;
+    virtual int hasTitle(const std::string userName, const u64 titleId) = 0;
+    virtual int queryTitles(const std::string userName, std::vector<u64>& outTitles) = 0;
+    virtual int queryRevisions(const std::string userName, const u64 titleId, std::vector<std::string>& outRevisions) = 0;
 };
 
 
-class RestRemoteStore : public IRemoteStoreIO
+class HTTPRemoteStore : public IRemoteStoreIO
 {
 private:
-    std::string m_serverUrl;
+    std::string serverUrl;
+    std::string saveDataPath;
 
 public:
-    RestRemoteStore(const std::string& serverUrl);
-    ~RestRemoteStore();
+    HTTPRemoteStore(const std::string& serverUrl, const std::string& saveDataPath);
+    ~HTTPRemoteStore();
+
+private:
+    int _issueSaveDataRevisionId(const std::string userName, const u64 titleId, std::string& revisionId);
+    int _uploadSaveDataRevision(const std::string userName, const u64 titleId, const std::string& revisionId);
+    int _getLatestSaveDataRevision(const std::string userName, const u64 titleId, std::string& revisionId);
+    int _downloadSaveDataRevision(const std::string userName, const u64 titleId, const std::string& revisionId);
 
 public:
-    int push(AccountUid accountUid, u64 titleId) override;
-    int pull(AccountUid accountUid, u64 titleId) override;
-    int push(AccountUid accountUid, u64 titleId, const std::string& revision) override;
-    int pull(AccountUid accountUid, u64 titleId, const std::string& revision) override;
+    int push(const std::string userName, const u64 titleId) override;
+    int pull(const std::string userName, const u64 titleId) override;
+    int push(const std::string userName, const u64 titleId, const std::string& revision) override;
+    int pull(const std::string userName, const u64 titleId, const std::string& revision) override;
 };
